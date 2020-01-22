788c622ca1cb476eefc58c3e1c68d6b40d414864
Johan <johan.a.cardell@gmail.com>
2020-01-22 15:58:41 +0100

    Insert function

--- Lab3DB/Lab3DB/Program.cs
+++ Lab3DB/Lab3DB/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using MongoDB.Driver;
+using System.Collections;
+using System.Collections.Generic;
+using Lab3DB.Models;
 
 namespace Lab3DB
 {
@@ -17,6 +20,15 @@ namespace Lab3DB
         {
             var client = new MongoClient();
             db = client.GetDatabase(database);
+        }
+
+        public void InsertRecord(List<Restaurant> restaurants)
+        {
+            var collection = db.GetCollection<Restaurant>("Restaurants");
+            foreach (var restaurant in restaurants)
+            {
+                collection.InsertOne(restaurant);
+            }
         }
     }
 }
