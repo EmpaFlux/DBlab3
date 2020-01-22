89994b9a54fb2894487885af3da0275725707adf
Emil <penzon88@gmail.com>
2020-01-22 14:07:12 +0100

    First commit

---
+++ Lab3DB/Lab3DB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab3DB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
