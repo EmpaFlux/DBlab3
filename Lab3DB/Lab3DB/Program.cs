e60244040ad31f1a872912df8c2889d6d9efa073
Emil <penzon88@gmail.com>
2020-01-23 15:20:22 +0100

    Debugging Rread

--- Lab3DB/Lab3DB/Program.cs
+++ Lab3DB/Lab3DB/Program.cs
@@ -3,6 +3,8 @@ using MongoDB.Driver;
 using System.Collections;
 using System.Collections.Generic;
 using Lab3DB.Models;
+using Newtonsoft.Json;
+using MongoDB.Bson;
 
 namespace Lab3DB
 {
@@ -10,7 +12,10 @@ namespace Lab3DB
     {
         static void Main(string[] args)
         {
-            
+            var mongoCRUD = new MongoCRUD("Restaurants");
+            mongoCRUD.InsertRecord(mongoCRUD.GetRestaurants());
+            var collection = mongoCRUD.ReadRestaurants();
+           
         }
     }
     public class MongoCRUD
@@ -22,13 +27,38 @@ namespace Lab3DB
             db = client.GetDatabase(database);
         }
 
-        public void InsertRecord(List<Restaurant> restaurants)
+        public void InsertRecord(List<BsonDocument> restaurants)
         {
-            var collection = db.GetCollection<Restaurant>("Restaurants");
-            foreach (var restaurant in restaurants)
+            var collection = db.GetCollection<BsonDocument>("Restaurants");
+            collection.InsertMany(restaurants);
+        }
+        public List<BsonDocument> GetRestaurants()
+        {
+            var list = new List<BsonDocument>()
             {
-                collection.InsertOne(restaurant);
+                new BsonDocument{ { "name", "Sun Bakery Trattoria" }, { "stars", 4 }, {"categories", new BsonArray{"Pizza", "Pasta", "Italian", "Coffee", "Sandwiches"} } },
+                new BsonDocument{ { "name", "Blue Bagels Grill" }, { "stars", 3 }, {"categories", new BsonArray{ "Bagels", "Cookies", "Sandwiches" } } },
+                new BsonDocument{ { "name", "Hot Bakery Cafe" }, { "stars", 4 }, {"categories", new BsonArray{ "Bakery", "Cafe", "Coffee", "Dessert" } } },
+                new BsonDocument{ { "name", "XYZ Coffee Bar" }, { "stars", 5 }, {"categories", new BsonArray{ "Coffee", "Cafe", "Bakery", "Chocolates" } } },
+                new BsonDocument{ { "name", "456 Cookies Shop" }, { "stars", 4 }, {"categories", new BsonArray{"Bakery", "Cookies", "Cake", "Coffee"} } }
+            };
+            return list;
+        }
+
+        public List<BsonDocument> ReadRestaurants()
+        {
+            var collection = db.GetCollection<BsonDocument>("Restaurants");
+            return collection.Find(new BsonDocument()).ToList();
+        }
+
+
+        public static void WriteDocuments(List<BsonDocument> restaurants)
+        {
+            foreach (var item in restaurants)
+            {
+                Console.WriteLine(item.ToString());
             }
         }
     }
+    
 }
